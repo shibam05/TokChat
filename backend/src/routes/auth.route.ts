a8d30198ece0d0bd7ef8aaa8d6dbfce455c51b0a
Shibam Biswas <sbiswas2004in@gmail.com>
2026-06-04 20:44:42 +0530

    Ready for deployment

--- backend/src/routes/auth.route.ts
+++ backend/src/routes/auth.route.ts
@@ -4,14 +4,14 @@ const router = express.Router();
 
 router.get("/signup", (req, res) => {
     res.send("signup endpoint")
-})
+});
 
 router.get("/login", (req, res) => {
     res.send("login endpoint")
-})
+});
 
 router.get("/logout", (req, res) => {
     res.send("logout endpoinjjjt")
-})
+});
 
 export default router;
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,16 +2,28 @@
 
 import express from 'express';
 import dotenv from 'dotenv';
-import authRoutes from "./routes/auth.route.ts"
-import messageRoutes from "./routes/message.route.ts"
+import path from 'path';
+import authRoutes from "./routes/auth.route.ts";
+import messageRoutes from "./routes/message.route.ts";
 
 dotenv.config();
 
 const app = express();
+const __dirname = path.resolve();
 const PORT = process.env.PORT || 3000;
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
+
+// deployment
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+    app.get("*", (_, res) => {
+        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"))
+    });
+}
+
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
