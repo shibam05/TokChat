e38792bd5f41a923ebf63a5f4acb9fd45b64ca02
Shibam Biswas <sbiswas2004in@gmail.com>
2026-06-05 22:14:02 +0530

    fix: update import statements to use .js extensions for routes

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -3,8 +3,8 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import path from 'path';
-import authRoutes from "./routes/auth.route.ts";
-import messageRoutes from "./routes/message.route.ts";
+import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
 
 dotenv.config();
 
